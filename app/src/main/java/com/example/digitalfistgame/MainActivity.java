ff16221335af3da92e5c0cde2bdd43336cdecddd
Karim Baba <240167470@stu.vtc.edu.hk>
2025-06-11 23:31:31 +0800

    initial push

---
+++ app/src/main/java/com/example/digitalfistgame/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.digitalfistgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnPlay, btnRecords, btnClose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnPlay = findViewById(R.id.btnPlay);
+        btnRecords = findViewById(R.id.btnRecords);
+        btnClose = findViewById(R.id.btnClose);
+
+        btnPlay.setOnClickListener( v ->{
+            //TODO: Replace GameActivity later on
+            Intent intent = new Intent(MainActivity.this, GameActivity.class)
+        });
+        btnRecords.setOnClickListener( v ->{
+            //TODO: Replace RecordsActivity later on
+            Intent intent = new Intent(MainActivity.this, RecordsActivity.class)
+        });
+        btnClose.setOnClickListener( v ->{
+            //TODO: Replace CloseActivity later on
+            finishAffinity(); //close app
+        });
+    }
+}
